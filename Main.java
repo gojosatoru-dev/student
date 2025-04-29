0e9d2a6e6b13b9d76fd599a389556e032268c556
snehakittur <snehakittur17@gmail.com>
2025-04-29 14:45:15 +0530

    commited by amit-dev

--- Main.java
+++ Main.java
@@ -1,36 +1,52 @@
-class Calculator{
-    int a ;
-    int b ;
+class Date {
+    int day;
+    int month;
+    int year;
 
-    Calculator(int a ,int b)
-    {
-        this.a=a;
-        this.a=a;        
+    // Constructor
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
-    int add(int a ,int b)
-    {
-    return a+b;
+
+    public int calculateAge(int currentDay, int currentMonth, int currentYear) {
+        int age = currentYear - year;
+        if (currentMonth < month || (currentMonth == month && currentDay < day)) {
+            age--; // Adjust age if birthday hasn't occurred yet in the current year
+        }
+        return age;
     }
-    int sub(int a ,int b)
-    {
-    return a-b;
-    }
-    int mult(int a ,int b)
-    {
-    return a*b;
-    }
-    int div(int a ,int b)
-    {
-    return a/b;
+
+    public String toString() {
+        return day + "-" + month + "-" + year;
     }
 }
-public class Main{
-    public static void main(String[] args){
-     Calculator calc= new Calculator(4, 5);
 
-     System.out.println("The addition of number is"+calc.add(4, 5));
-     System.out.println("The substraction of number is"+calc.sub(4, 5));
-     System.out.println("The division of number is"+calc.div(4, 5));
-     System.out.println("The multiplication of number is"+calc.mult(4, 5));
+class Student {
+    String name;
+    Date dob;
+
+    // Constructor
+    public Student(String name, int day, int month, int year) {
+        this.name = name;
+        this.dob = new Date(day, month, year);
+    }
+
+    public void displayDetails(int currentDay, int currentMonth, int currentYear) {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + dob.calculateAge(currentDay, currentMonth, currentYear));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Example input: Date of Birth in DD-MM-YYYY format
+        Student student = new Student("Amit", 01, 01, 2005);
+
+        // Set the current date manually
+        int currentDay = 29, currentMonth = 4, currentYear = 2025;
+
+        student.displayDetails(currentDay, currentMonth, currentYear);
     }
 }
